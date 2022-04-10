36994b9777828d40da40d35f9249a35debcfd2ba
Braeden <bvenne@my.bcit.ca>
2022-04-10 16:19:03 -0700

    Added a function to app.js that adds an event listener to each of the note titles in the side nav list

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const textboxTemp = `<textarea id='create-note' rows='30' cols='50'></textarea>`
 const saveBtnTemp = `<button id='save'>Save</button>`
 const cancelBtnTemp = `<button id='cancel'>Cancel</button>`
-const createNotes = document.querySelector('.create-note-area')
+const createNotes = document.querySelector('.write-note-area')
 const btn = document.querySelector('.fa-circle-plus')
 
 
@@ -12,7 +12,6 @@ const notes = [
     id: 1 
   }
 ]
-
 
 
 function createNotesArea() {
@@ -27,6 +26,7 @@ function createNotesArea() {
   saveBtn.addEventListener('click', saveNote)
 }
 
+
 function cancelNote(evt) {
   const textbox = document.querySelector('#create-note')
   const savebtn = document.querySelector('#save')
@@ -37,22 +37,43 @@ function cancelNote(evt) {
   cancelbtn.remove()
 }
 
+
 function saveNote(evt) {
   const textbox = document.querySelector('#create-note')
-  var text = textbox.value.split('\n')
-  var textTitle = text[0]
-  var textBody = text.slice(1)
+  let text = textbox.value.split('\n')
+  let textTitle;
+  if (text[0] == '') {
+    textTitle = 'No Title'
+  } else {
+    textTitle = text[0]
+  }
   
-  notes.push({title: textTitle, noteBody: textBody, id:notes.length + 1})
+  let textBody = text.slice(1)
+  let id = notes.length + 1
+  notes.push({title: textTitle, noteBody: textBody, id: id})
   console.log(notes)
   noteTitles(textTitle)
+  noteTitleBtn(id)
   cancelNote(evt)
 }
 
+
 function noteTitles(evt) {
+  const noteID = notes[notes.length - 1]
   const navBar = document.querySelector('.notes-list')
-  const titles = `<li id='noteList'>${evt}</li>`
+  let titles = `<li id=${noteID.id}>${noteID.title}</li>`
   navBar.insertAdjacentHTML('afterbegin', titles)
 }
+
+
+function noteTitleBtn(id) {
+  const noteTitles = document.getElementById(id)
+  noteTitles.addEventListener('click', readNotes)
+}
+
+function readNotes() {
+  alert('Test')
+}
+
 
 btn.addEventListener('click', createNotesArea)
