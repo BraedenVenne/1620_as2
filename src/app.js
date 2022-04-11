5aee24817fef251a05a0ce9431386bd65102b644
Braeden <bvenne@my.bcit.ca>
2022-04-11 14:50:01 -0700

    added a function to app.js that toggles dark theme when clicking the checkbox

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
+//Created templates
 const textboxTemp = `<textarea id='create-note' rows='30' cols='50'></textarea>`
 const saveBtnTemp = `<button id='save'>Save</button>`
 const cancelBtnTemp = `<button id='cancel'>Cancel</button>`
 const closeBtnTemp = `<button id='close'>Close</button>`
+const hTemp = `<h1></h1>`
+const pTemp = `<p></p>`
+
 const createNotes = document.querySelector('.write-note-area')
-const btn = document.querySelector('.fa-circle-plus')
+const noteBtn = document.querySelector('.fa-circle-plus')
 
-
-
+//Array used for storing note titles, body, and ids
 const notes = [
   { 
     title: "first note", 
@@ -15,7 +18,7 @@ const notes = [
   }
 ]
 
-
+//Creates a textbox with a save and cancel button
 function createNotesArea() {
   createNotes.insertAdjacentHTML('afterend', cancelBtnTemp)
   createNotes.insertAdjacentHTML('afterend', saveBtnTemp)
@@ -28,7 +31,7 @@ function createNotesArea() {
   saveBtn.addEventListener('click', saveNote)
 }
 
-
+//Closes the note area after clicking the Cancel button
 function cancelNote(evt) {
   const textbox = document.querySelector('#create-note')
   const savebtn = document.querySelector('#save')
@@ -39,7 +42,7 @@ function cancelNote(evt) {
   cancelbtn.remove()
 }
 
-
+//Saves the note after clicking the Save button
 function saveNote(evt) {
   const textbox = document.querySelector('#create-note')
   let text = textbox.value.split('\n')
@@ -59,7 +62,7 @@ function saveNote(evt) {
   cancelNote(evt)
 }
 
-
+//Stores the note titles in the side nav list
 function noteTitles(evt) {
   const noteID = notes[notes.length - 1]
   const navBar = document.querySelector('.notes-list')
@@ -67,28 +70,48 @@ function noteTitles(evt) {
   navBar.insertAdjacentHTML('afterbegin', titles)
 }
 
-
+//Adds event listeners to the titles in the list
 function noteTitleBtn(id) {
   const noteTitles = document.getElementById(id)
   noteTitles.addEventListener('click', readNotes)
 }
 
+//Opens the saved notes and adds a close button
 function readNotes() {
-  const newNote = notes[notes.length - 1]
-  const textAreaTemp = `<textarea id='create-note' rows='30' cols='50'>${newNote.noteBody}</textarea>`
   createNotes.insertAdjacentHTML('afterend', closeBtnTemp)
-  createNotes.insertAdjacentHTML('afterend', textAreaTemp)
+
+  
+
+  const newNote = notes[notes.length - 1]
+  let noteTitle = `<h1 id=${newNote.id}>${newNote.title}</h1>`
+  let note = `<p id=${newNote.id}>${newNote.noteBody}</p>`
+  createNotes.insertAdjacentHTML('afterend', noteTitle)
+  createNotes.insertAdjacentHTML('afterend', note)
 
   const closeBtn = document.querySelector('#close')
   closeBtn.addEventListener('click', closeNote)
 }
 
-function closeNote() {
-  const textbox = document.querySelector('#create-note')
-  const closebtn = document.querySelector('#close')
-  
-  textbox.remove()
-  closebtn.remove()
+//Closes the notes after pressing the Close button
+function closeNote(evt) {
+  evt.currentTarget.remove()
 }
 
-btn.addEventListener('click', createNotesArea)
+//Opens the note area after pressing the plus button
+noteBtn.addEventListener('click', createNotesArea)
+
+//Bonus
+//Toggles dark-theme
+function toggleDarkMode(evt) {
+  const mainContainer = document.querySelector('.main-container')
+  if (evt.checked == true) {
+    mainContainer.classList.toggle('light-theme')
+  } else {
+    mainContainer.classList.toggle('dark-theme')
+  }
+
+
+}
+
+const themeBtn = document.querySelector('.theme-toggle')
+themeBtn.addEventListener('click', toggleDarkMode)
